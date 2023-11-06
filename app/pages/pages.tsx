bc83a09b9123e93371da7da3c5072bd92d8b6860
RemyXVX <remyfranciscoflores@gmail.com>
2023-11-06 10:46:30 -0800

    saved rendered page to local storage for when pages refresh

--- app/pages/pages.tsx
+++ app/pages/pages.tsx
@@ -1,12 +1,23 @@
 'use client'
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import HomePage from "./home/page";
 import SideNav from "../components/sidenav/page";
 
 const Main = () => {
   const [selectedPage, setSelectedPage] = useState('home');
+  
+  useEffect(() => {
+    const savedPage = localStorage.getItem('currentPage');
+    if (savedPage) {
+      setSelectedPage(savedPage);
+    }
+  }, []);
 
+  useEffect(() => {
+    localStorage.setItem('currentPage', selectedPage);
+  }, [selectedPage]);
+  
   return (
     <div className="grid grid-cols-6 sm:grid-cols-8 md:grid-cols-10 lg:grid-cols-12">
       <div className="col-span-1 md:col-span-1">
